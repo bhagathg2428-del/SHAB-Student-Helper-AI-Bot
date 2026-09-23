8e9001600932b3409363b567df7f130641d78722
bhagathg2428-del <bhagath.g.2428@gmail.com>
2026-09-23 20:25:27 +0530

    Fix UUID generation for user ID

--- backend/models/models.py
+++ backend/models/models.py
@@ -7,7 +7,7 @@ from backend.database.database import Base
 class User(Base):
     __tablename__ = "users"
 
-    id = Column(String, primary_key=True, index=True,default=lambda:str(uuid.uuid44()))
+    id = Column(String, primary_key=True, index=True,default=lambda:str(uuid.uuid4()))
     full_name = Column(String, nullable=False)
     email = Column(String, unique=True, index=True, nullable=False)
     password_hash = Column(String, nullable=False)
