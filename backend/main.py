97e640901f2a5b40816dabb8c034e1fa337198e0
bhagathg2428-del <bhagath.g.2428@gmail.com>
2026-09-23 21:46:24 +0530

    Fix User and note model import

--- backend/main.py
+++ backend/main.py
@@ -9,7 +9,7 @@ from jose import jwt, JWTError
 from passlib.context import CryptContext
 
 from backend.database.database import engine, Base, SessionLocal
-from backend.models.models import Note
+from backend.models.models import User,Note
 from backend.schemas.schemas import UserRegister, UserLogin, UserOut
 
 
